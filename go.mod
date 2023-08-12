27593ab75750e2d17b3f7ff381db30dc72792c2b
284045796@qq.com <284045796@qq.com>
2023-08-12 23:33:30 +0800

    first mod run ok

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module testgo
 
+// replace github.com/hirryli/testgo/test => ./test
 go 1.18
--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"testgo/test"
 )
+
+type BB interface {
+	SetName(string)
+	GetName() string
+	SetTotal(int)
+	GetTotal() int
+}
 
 func main() {
 	fmt.Println("Hello the world!")
+	var book test.Book
+	book.Name = "test"
+	var tt BB
+	tt = book
+	// var tt test.Book
+	// tt.SetName("tetsst")
+	fmt.Println("tt.Name:" + tt.GetName())
 }
